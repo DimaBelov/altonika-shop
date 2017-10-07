7a53cdef14cfb51b5bf56e1dc1a4bcd253a754c0
WILDBERRIES\Belov.Dmitriy3 <belov.dmitriy3@wildberries.ru>
2017-10-07 18:37:54 +0300

    удалил version1

--- src/lib/paginator-panel/paginator-panel.ts
+++ src/lib/paginator-panel/paginator-panel.ts
@@ -31,48 +31,6 @@ export class PaginatorPanel implements OnAll {
 
     }
 
-    version1 () {
-        if (this.pageNumbersContainerTest == null) {
-            return;
-        }
-        let html = '';
-        let currentPage = this.paginator.paggingOptions.pageNumber;
-        let resultPageNumbers = this.paginator.paggingResult.pageNumbers;
-
-        let i: number;
-        let bottom = currentPage - 1;
-        let top = currentPage + 1;
-
-        if (currentPage === resultPageNumbers.length) {
-            bottom = currentPage - 2;
-            top = currentPage;
-        }
-
-        for (i = bottom; i <= top; i++) {
-            if (i === 0 || i >= resultPageNumbers.length) {
-                continue;
-            }
-            html += this.getButton(i);
-            Logger.log(html);
-        }
-
-        let last = resultPageNumbers[resultPageNumbers.length - 1];
-
-        if (i < last) {
-            html += '<p>...</p>';
-        }
-
-        html += this.getButton(last);
-
-        this.pageNumbersContainerTest.nativeElement.innerHTML = html;
-
-        let btns = this.pageNumbersContainerTest.nativeElement.getElementsByTagName('button');
-        for (let j = 0; j < btns.length; j++) {
-            let btn = $(btns[j]);
-            btn.click(() => this.paginator.selectPage(+btn.attr('data-value')));
-        }
-    }
-
     ngAfterViewInit() {
         if (this.pageNumbersContainerTest == null) {
             return;
