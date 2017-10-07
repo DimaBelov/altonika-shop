3a2ad8228099a31633d04a0bd6cbdaf7a17cd956
WILDBERRIES\Belov.Dmitriy3 <belov.dmitriy3@wildberries.ru>
2017-10-07 06:35:56 +0300

    paginator 2

--- src/services/paginator.ts
+++ src/services/paginator.ts
@@ -1,34 +1,49 @@
+import {Observable} from 'rxjs/Rx';
+
 import { PaggingOptions } from '@entities/pagging-options';
 import { PaggingResult } from '@entities/pagging-result';
+import { AsyncCommand } from '@lib/async-command';
 
 export class Paginator<T> {
+    pageSizeOptions = [12, 24, 36, 48];
     paggingOptions: PaggingOptions = {
         pageNumber: 1,
         pageSize: 12,
         searchText: ''
     };
     paggingResult: PaggingResult<T>;
+    pageCommand: AsyncCommand<PaggingResult<T>>;
 
     constructor(
-        private onPaggingChanged: (paggingOptions: PaggingOptions) => PaggingResult<T>,
-
+        private onPaggingChanged: (paggingOptions: PaggingOptions) => Observable<PaggingResult<T>>
     ) {
+        this.pageCommand = new AsyncCommand<PaggingResult<T>>(
+            () => this.onPaggingChanged(this.paggingOptions),
+            (r, e) => this.pageComplete(r, e)
+        );
+    }
 
+    pageComplete(result: PaggingResult<T>, error: any) {
+        if (error != null) {
+            return;
+        }
+
+        this.paggingResult = result;
     }
 
     prevPage() {
         this.paggingOptions.pageNumber -= 1;
-        this.paggingResult = this.onPaggingChanged(this.paggingOptions);
+        this.pageCommand.execute();
     }
 
     nextPage() {
         this.paggingOptions.pageNumber += 1;
-        this.paggingResult = this.onPaggingChanged(this.paggingOptions);
+        this.pageCommand.execute();
     }
 
     firstPage() {
         this.paggingOptions.pageNumber = 1;
-        this.paggingResult = this.onPaggingChanged(this.paggingOptions);
+        this.pageCommand.execute();
     }
 
     lastPage() {
@@ -37,6 +52,6 @@ export class Paginator<T> {
                 this.paggingResult.pageNumbers.length - 1,
                 this.paggingResult.pageNumbers.length)
             [0];
-        this.paggingResult = this.onPaggingChanged(this.paggingOptions);
+        this.pageCommand.execute();
     }
 }
