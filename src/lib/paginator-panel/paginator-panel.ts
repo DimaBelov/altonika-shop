2863a51a1406e0c5d1b46e852fa66b2f4cd2ffb0
WILDBERRIES\Belov.Dmitriy3 <belov.dmitriy3@wildberries.ru>
2017-10-07 18:37:30 +0300

    pagging

--- src/lib/paginator-panel/paginator-panel.ts
+++ src/lib/paginator-panel/paginator-panel.ts
@@ -31,7 +31,7 @@ export class PaginatorPanel implements OnAll {
 
     }
 
-    ngAfterViewInit() {
+    version1 () {
         if (this.pageNumbersContainerTest == null) {
             return;
         }
@@ -46,6 +46,49 @@ export class PaginatorPanel implements OnAll {
         if (currentPage === resultPageNumbers.length) {
             bottom = currentPage - 2;
             top = currentPage;
+        }
+
+        for (i = bottom; i <= top; i++) {
+            if (i === 0 || i >= resultPageNumbers.length) {
+                continue;
+            }
+            html += this.getButton(i);
+            Logger.log(html);
+        }
+
+        let last = resultPageNumbers[resultPageNumbers.length - 1];
+
+        if (i < last) {
+            html += '<p>...</p>';
+        }
+
+        html += this.getButton(last);
+
+        this.pageNumbersContainerTest.nativeElement.innerHTML = html;
+
+        let btns = this.pageNumbersContainerTest.nativeElement.getElementsByTagName('button');
+        for (let j = 0; j < btns.length; j++) {
+            let btn = $(btns[j]);
+            btn.click(() => this.paginator.selectPage(+btn.attr('data-value')));
+        }
+    }
+
+    ngAfterViewInit() {
+        if (this.pageNumbersContainerTest == null) {
+            return;
+        }
+        let html = '';
+        let currentPage = this.paginator.paggingOptions.pageNumber;
+        let resultPageNumbers = this.paginator.paggingResult.pageNumbers;
+
+        let i: number;
+        let bottom = currentPage;
+        let top = currentPage + 2;
+        let diff = resultPageNumbers.length - currentPage;
+
+        if (diff <= 2) {
+            bottom = currentPage - (2 - diff + 1);
+            top = currentPage + diff;
         }
 
         for (i = bottom; i <= top; i++) {
