c651772cc7603c142f2caf81064f1761239a6b71
WILDBERRIES\Belov.Dmitriy3 <belov.dmitriy3@wildberries.ru>
2017-10-08 12:20:51 +0300

    commit

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -5,6 +5,7 @@ import { Product } from '@entities/product';
 import { ProductService } from '@services/product.service';
 import { BasketService } from '@services/basket.service';
 import { ListComponent } from '../list/list.component';
+import { FavoritesService } from '@services/favorites.service';
 
 @Component({
   selector: 'app-product-card',
@@ -20,7 +21,8 @@ export class ProductCardComponent implements OnInit {
     private _route: ActivatedRoute,
     private _router: Router,
     private _productService: ProductService,
-    public basketService: BasketService) {
+    public basketService: BasketService,
+    public favoritesService: FavoritesService) {
     _route.queryParams.subscribe(params => {
       this.productId = params['id'];
       this.refresh();
@@ -32,6 +34,7 @@ export class ProductCardComponent implements OnInit {
     .subscribe(
       p => {
         this.product = p;
+        this.product.isFavorite = this.favoritesService.contains(this.product);
       },
       error => {
         console.log('get product by id error');
