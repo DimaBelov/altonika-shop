079f14ecb6798f4d97e96944b4eb924d969228f2
DimaBelov <dimabelovmail@gmail.com>
2017-10-09 09:21:56 +0300

    вынес remote-script в отдельный файл

--- scripts/postpublish.js
+++ scripts/postpublish.js
@@ -1,13 +1,7 @@
 var spawn = require('child_process').spawn;
 var child =
     spawn(
-        '(echo mkdir -p altonika-shop/frontend ' +
-        '&& echo cd altonika-shop/frontend ' +
-        '&& echo rm -r ./*' +
-        '&& echo put -r C:/Git/GitHub/altonika-shop/dist/.)' +
-        '>remote-script && ' +
-        'psftp u2944939@185.26.114.186 -b remote-script -be && ' +
-        'del remote-script', {
+        'psftp u2944939@185.26.114.186 -b remote-script -be', {
             windowsVerbatimArguments: true,
             shell: true,
             stdio: 'inherit'
