0be78902d8e1d3b69fc742235acd19118756aa25
WILDBERRIES\Belov.Dmitriy3 <belov.dmitriy3@wildberries.ru>
2017-10-09 17:23:50 +0300

    bash script

--- scripts/postpublish.js
+++ scripts/postpublish.js
@@ -1,8 +1,34 @@
 var spawn = require('child_process').spawn;
-var child =
-    spawn(
-        'psftp u2944939@185.26.114.186 -b remote-script -be', {
-            windowsVerbatimArguments: true,
-            shell: true,
-            stdio: 'inherit'
-        });
+var fileExists = require('file-exists');
+
+var puttyScript = 'psftp u2944939@185.26.114.186 -b putty-remote-script -be';
+var bashScript = 'bash -c "echo Delete old files && ssh 185.26.114.186 -l u2944939 \'rm -rf altonika-shop/frontend/* && exit\' && echo Push new files && scp -r /mnt/c/GitHub/altonika-shop/dist/. u2944939@185.26.114.186:/home/u2944939/altonika-shop/frontend"';
+
+fileExists('C:/Windows/System32/bash.exe').then(exists => {
+    if (exists) {
+        console.log('Run bash script');
+        runBashScript();
+    } else {
+        console.log('Run putty script');
+        runPuttyScript();
+    }
+});
+
+runBashScript = function () {
+    var child =
+        spawn(
+            bashScript, {
+                shell: true,
+                stdio: 'inherit'
+            });
+}
+
+runPuttyScript = function () {
+    var child =
+        spawn(
+            puttyScript, {
+                windowsVerbatimArguments: true,
+                shell: true,
+                stdio: 'inherit'
+            });
+}
